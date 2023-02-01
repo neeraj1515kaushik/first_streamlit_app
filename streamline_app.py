440c65604a9991dfd9ee7542f1e5dfeb3ffd8441
Neeraj Kaushik <36379993+neeraj1515kaushik@users.noreply.github.com>
2023-02-02 00:57:41 +0530

    Update streamline_app.py
    added breakfast menu

--- streamline_app.py
+++ streamline_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title("My family's new happy diner!")
+streamlit.header("Breakfast Menu")
+streamlit.text("🍜Omega 3 and blueberry oatmeal")
+streamlit.text("🍲kale, spinach and rocket smoothie")
+streamlit.text("🍳Hard boiled free-range egg")
+streamlit.text("🥑Avocado toast")
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
