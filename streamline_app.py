fde96127c364fccc76abdb2cfaa7a4be064af681
Neeraj Kaushik <36379993+neeraj1515kaushik@users.noreply.github.com>
2023-02-02 08:43:20 +0530

    Update streamline_app.py
    adding data frame of fruit list to it.

--- streamline_app.py
+++ streamline_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+import pandas
 streamlit.title("My family's new happy diner!")
 streamlit.header("Breakfast Menu")
 streamlit.text("🍜Omega 3 and blueberry oatmeal")
@@ -9,3 +9,6 @@ streamlit.text("🥑Avocado toast")
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
