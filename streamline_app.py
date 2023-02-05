511cf8c6363fb69630339f6082147c7c417fb72b
Neeraj Kaushik <36379993+neeraj1515kaushik@users.noreply.github.com>
2023-02-05 17:56:26 +0530

    Update streamline_app.py
    adding fruit_choice

--- streamline_app.py
+++ streamline_app.py
@@ -41,3 +41,6 @@ my_data_row = my_cur.fetchall()
 
 streamlit.text("The loaded fruit list items are:")
 streamlit.dataframe(my_data_row)
+
+fruit_choice = streamlit.text_input('What fruit would you like to add?')
+streamlit.text("Thanks for adding " + fruit_choice)
