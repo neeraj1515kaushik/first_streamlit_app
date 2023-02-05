de25736fd295d14e0092cb86d4d775e2da875370
Neeraj Kaushik <36379993+neeraj1515kaushik@users.noreply.github.com>
2023-02-05 09:49:44 +0530

    Update streamline_app.py
    fruit choice

--- streamline_app.py
+++ streamline_app.py
@@ -24,6 +24,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #fruityvice_normalized = fruityvice_response.json()
 streamlit.text(fruityvice_response.json())
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # converting into dataframe
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
